0886c55984413d5bcab5493faf0d53f70b9e8de0
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-12-07 13:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function showConfirmationDialog() {
+  // Display the confirmation dialog
+  const userClickedOk = confirm("Do you want to proceed?");
+
+  // Check the user's choice
+  if (userClickedOk) {
+    alert("You clicked OK. Proceeding...");
+  } else {
+    alert("You clicked Cancel. Exiting...");
+  }
+}
+
+
+showConfirmationDialog();
